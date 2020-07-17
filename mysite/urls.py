b1ec40ae1b7b79d52092cd1c1076e83352d048d1
xinhong3 <jujugregoryh@gmail.com>
2020-07-17 10:12:17 +0800

    Using Django's form

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
     path('login/', views.login),
     path('register/', views.register),
     path('logout/', views.logout),
+    path('captcha/', include('captcha.urls'))
 ]
